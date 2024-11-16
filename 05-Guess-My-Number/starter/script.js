30061d90ebc794e892c76961102440289ec5a893
linkiratanga <linkiratanga@gmail.com>
2024-11-16 05:47:37 +0200

    Big Dawg beginning

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+/*
+document.querySelector('.message').textContent = 'Correct number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;  
+*/
+const secretNumber = Math.trunc(Math.random() * 6) + 1;
+let score = 20;
+let highScore = 0;
+const messageDisplay = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const scoreDisplay = function (scoreResult) {
+  document.querySelector('.score').textContent = scoreResult;
+};
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // no input
+  if (!guess) {
+    messageDisplay('No number');
+    // When Player wins
+  } else if (secretNumber === guess) {
+    messageDisplay('You got it 👌');
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '300px';
+    scoreDisplay(secretNumber);
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    // When guess is different
+  } else if (guess != secretNumber) {
+    if (score > 1) {
+      messageDisplay(
+        guess < secretNumber ? 'Number is too low 📉' : 'Number is too high 📈 '
+      );
+
+      score--;
+      scoreDisplay(score);
+    } else if (score) {
+      messageDisplay('Game is over 🔥');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#333';
+  document.querySelector('.number').style.width = '15rem';
+  messageDisplay('Start guessing...');
+  scoreDisplay(score)
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// When multiple class at once they are represented as Array
+console.log(btnsOpenModal);
+
+for (let i = 0; i < 3; i++) {
+  // console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', modalOpener);
+}
+
+function modalOpener() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const modalCloser = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', modalCloser);
+// Paratheses are remove to prevent js to directly call that fonction without Listening the Event
+
+overlay.addEventListener('click', modalCloser);
+// in strict mode strick equality operator are crucial
+document.addEventListener('keydown', function (press) {
+  if (press.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modalCloser();
+  }
+  console.log(modal.classList.contains('hidden'));
+  console.log(`You clicked on key ${press.key}`);
+  // console.log(`You clicked button ${[key]}`);
+}); // listen every key press
+
+// KeyboardEvent {isTrusted: true, key: 'a', code: 'KeyA', location: 0, ctrlKey: false, …}
